6423ad8bd5dcf024fb63a1bbca14330fd36da640
aziegle6@jh.edu <csf22006@ugradx.cs.jhu.edu>
2022-11-28 11:09:48 -0500

    pawn legal move and legal capture shape

---
+++ Pawn.cpp
@@ -0,0 +1,49 @@
+#include "Pawn.h"
+
+namespace Chess
+{
+    bool legal_move_shape(const Position& start, const Position& end) const {
+        if (start.first == end.first && start.second == end.second) {
+            return false;
+        }
+        if(is_white){
+            if(start.second == 2){
+                if((end.second - start.second == 2 || end.second - start.second == 1) && start.first == end.first){
+                    return true;
+                }
+            } else {
+                if(end.second - start.second == 1 && start.first == end.first){
+                    return true;
+                }
+            }
+            return false;
+        }else {
+            if(start.second == 7){
+                if((start.second - end.second == 2 || start.second - end.second == 1) && start.first == end.first){
+                    return true;
+                }
+            } else {
+                if(start.second - end.second == 1 && start.first == end.first){
+                    return true;
+                }
+            }
+            return false;
+        }   
+    } 
+    bool legal_capture_shape(const Position& start, const Position& end) const {
+        if (start.first == end.first && start.second == end.second) {
+            return false;
+        }
+        if(is_white){
+            if(end.second - start.second == 1 && abs(end.first - start.first) == 1){
+                return true;
+            }
+            return false;
+        }else {
+            if(start.second - end.second == 1 && abs(end.first - start.first) == 1){
+                return true;
+            }
+            return false;
+        }  
+    }
+}
