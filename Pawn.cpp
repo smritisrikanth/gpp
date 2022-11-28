12d4b712c165a608c0f79d1628673e8628d5d93e
Smriti Srikanth <ssrikan2@jhu.edu>
2022-11-28 11:18:18 -0500

    fixed errors

--- Pawn.cpp
+++ Pawn.cpp
@@ -2,11 +2,11 @@
 
 namespace Chess
 {
-    bool legal_move_shape(const Position& start, const Position& end) const {
+    bool Pawn::legal_move_shape(const Position& start, const Position& end) const {
         if (start.first == end.first && start.second == end.second) {
             return false;
         }
-        if(is_white){
+        if(Pawn::is_white){
             if(start.second == 2){
                 if((end.second - start.second == 2 || end.second - start.second == 1) && start.first == end.first){
                     return true;
@@ -30,11 +30,11 @@ namespace Chess
             return false;
         }   
     } 
-    bool legal_capture_shape(const Position& start, const Position& end) const {
+    bool Pawn::legal_capture_shape(const Position& start, const Position& end) const {
         if (start.first == end.first && start.second == end.second) {
             return false;
         }
-        if(is_white){
+        if(Pawn::is_white){
             if(end.second - start.second == 1 && abs(end.first - start.first) == 1){
                 return true;
             }
